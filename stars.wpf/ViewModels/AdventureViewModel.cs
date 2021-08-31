98511fd46736bec94600d0f4027b674f51e44f50
sobik.sebastian@gmail.com <sobik.sebastian@gmail.com>
2021-08-31 21:38:50 +0200

    done

--- stars.wpf/ViewModels/AdventureViewModel.cs
+++ stars.wpf/ViewModels/AdventureViewModel.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using stars.database;
+using stars.database.entities;
 using stars.database.repositories;
 using stars.wpf.Commands;
 using stars.wpf.Views;
@@ -34,6 +36,83 @@ namespace stars.wpf.ViewModels
 
         public void Action(object obj)
         {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            if(CurrentFleet > 0)
+            {
+                if (randomNumber < 20)
+            {
+                MessageBox.Show("Natrafiłeś na rój asteroid, tracisz 10 statków");
+
+                if (CurrentFleet >= 10)
+                {
+                    CurrentFleet = CurrentFleet - 10;
+                }
+                else
+                {
+                    CurrentFleet = 0;
+                }
+                User updateUser = LoginView.currentUser;
+                updateUser.Fleet = CurrentFleet;
+                UsersRepository.Update(updateUser);
+            }
+
+            else if (randomNumber is >= 20 and < 60)
+            {
+                MessageBox.Show("Pusta przestrzeń");
+            }
+
+            else if (randomNumber is >=60 and > 70)
+            {
+                MessageBox.Show("Natrafiłeś na kosmicznych piratów - Przygotuj się do walki!");
+                Random randomEnemy = new Random();
+                int randomEnemyFleet = randomEnemy.Next(0, 21);
+
+                if (randomEnemyFleet > CurrentFleet)
+                {
+                    MessageBox.Show("Przegrałeś! Tracisz wszystkie statki");
+                    CurrentFleet = 0;
+                }
+
+                else if (randomEnemyFleet == CurrentFleet)
+                {
+                    MessageBox.Show("Bitwa nierozstrzygnięta! Udało Ci się uciec ale tracisz 5 statków");
+
+                    if (CurrentFleet >= 5)
+                    {
+                        CurrentFleet = CurrentFleet - 5;
+                    }
+                    else
+                    {
+                        CurrentFleet = 0;
+                    }
+
+                    User updateUser = LoginView.currentUser;
+                    updateUser.Fleet = CurrentFleet;
+                    UsersRepository.Update(updateUser);
+                }
+
+                else
+                {
+                    MessageBox.Show("Wygrałeś! Piraci zostali rozbici");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Trafiłeś na ruiny opuszczonej stacji kosmicznej! dostajesz 100 sztuk stali");
+                CurrentStock = CurrentStock + 100;
+                User updateUser = LoginView.currentUser;
+                updateUser.Stock = CurrentStock;
+                UsersRepository.Update(updateUser);
+            }
+
+            }
+
+            else
+            {
+                MessageBox.Show("Nie masz floty!");
+            }
 
         }
     }
